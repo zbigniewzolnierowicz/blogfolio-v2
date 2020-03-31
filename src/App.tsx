d2f54cdb8b4720a5860c0fdee7afbef3c9288a32
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-03-31 18:33:51 +0200

    Add Reach Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Router, Link, RouteComponentProps } from "@reach/router"
+
+const TestA = ({ path }: RouteComponentProps) => <p>A</p>;
+const TestB = ({ path }: RouteComponentProps) => <p>B</p>;
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <nav>
+        <Link to="/">Home</Link> <Link to="b">B</Link>
+      </nav>
+      <main>
+        <Router>
+          <TestA path="/" />
+          <TestB path="b" />
+        </Router>
+      </main>
+      <footer>
+        &copy; Zbigniew Żołnierowicz {new Date().getFullYear()}
+      </footer>
+    </>
   );
 }
 

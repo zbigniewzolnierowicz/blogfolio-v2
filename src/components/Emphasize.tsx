3da3c9c6ab01a3dc15999771ac14b519b1c615fe
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-04-02 12:52:13 +0200

    Fix TS error

--- src/components/Emphasize.tsx
+++ src/components/Emphasize.tsx
@@ -2,9 +2,10 @@ import styled from "@emotion/styled"
 import { themeInterface } from "../helpers/EmotionTheme";
 
 interface EmphasizePropsInterface {
-    theme: themeInterface
+    theme: themeInterface,
+    as?: string
 }
 
 export const Emphasize = styled.span<EmphasizePropsInterface>`
-    text-decoration: dashed underline ${(props) => props.theme.colors.primary};
+    text-decoration: solid underline ${(props) => props.theme.colors.primary};
 `

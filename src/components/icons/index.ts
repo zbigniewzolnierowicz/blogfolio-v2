c91eb4f79ade00c5f192be9338b9a7e1c4f572d5
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2020-04-02 13:25:36 +0200

    Add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+import { css } from "@emotion/core";
+export const IconStyleHelper = css`
+    display: inline;
+    height: 1em;
+    margin: 0;
+    margin-bottom: -0.18em;
+`
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,11 @@
 /** @jsx jsx */
-import { useState } from "react";
 import { jsx, css } from '@emotion/core'
 import { RouteComponentProps } from '@reach/router'
 import { Emphasize } from '../components/Emphasize'
 import { motion } from "framer-motion"
 import styled from "@emotion/styled";
-
-
-const Main = styled.main`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+import { Typescript } from "../components/icons/Typescript"
+import { Firebase } from "../components/icons/Firebase"
 
 const Head = motion.custom(styled.div`
 text-align: center;
@@ -24,20 +18,37 @@ h2 {
 }
 `)
 
+const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    p {
+        text-align: justify;
+    }
+`
+
 export const Home = (_props: RouteComponentProps) => {
-    const [scale, setScale] = useState(1);
     return (
         <Main>
             <Head
-                initial={{ y: 200, rotateX: "-90deg" }}
-                animate={{ y: 0, rotateX: 0, scale }}
-                onTap={() => setScale(scale === 1 ? 1.3 : 1)}
-                whileTap={{ scale: 1.1 }}
+                initial={{ y: 200, rotateX: "-90deg", scale: 0 }}
+                animate={{ y: 0, rotateX: 0, scale: 1 }}
             >
                 <h1 css={css`font-size: 3em;`}><Emphasize>Zbigniew Żołnierowicz</Emphasize></h1>
-                <h2>A web developer with a penchant for <Emphasize>grilled cheese</Emphasize></h2>
+                <h2>A web developer with a penchant for <Emphasize>grilled cheese</Emphasize>  <span role="img" aria-label="cheese">🧀</span></h2>
             </Head>
+            <Section>
+                <Emphasize as="h3" css={css`font-size: 1.4em`}>
+                    What do I do?
+                </Emphasize>
+                <p>I'm a <Emphasize>front-end developer</Emphasize> specializing in JavaScript single-page applications written in <Typescript /> Typescript,  and JAMstack development using state-of-the-art serverless solutions, like <Firebase /> <Emphasize>Google Firebase</Emphasize>.</p>
+            </Section>
         </Main>
-
     )
 }
